c1dfae47d84b757ae521503d4353a26e313a128d
Angular CLI <angular-cli@angular.io>
2017-08-21 19:15:08 +0200

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'amte-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'amte';
+}
