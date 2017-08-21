3981c9bbf9a0629b34f5102520e52f23aabd3cff
tomastrajan <tomas.trajan@gmail.com>
2017-08-21 21:44:07 +0200

    feat: material themes example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/material';
+
 
 @Component({
   selector: 'amte-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'amte';
+
+  theme = 'my-theme';
+  constructor(
+    private overlayContainer: OverlayContainer
+  ) {}
+
+  ngOnInit(): void {
+    this.overlayContainer.themeClass = this.theme;
+  }
+
+  onThemeChange() {
+    this.overlayContainer.themeClass = this.theme;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule, MdButtonModule, MdSelectModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ParentComponent } from './example/parent/parent.component';
+import { ChildComponent } from './example/child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdButtonModule,
+    MdSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
